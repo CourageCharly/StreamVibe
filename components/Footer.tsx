7ae724c77da9009f9098fde3e214b67388a6a0a0
Courage Charly <couragecharly1@gmail.com>
2026-08-26 08:27:48 +0100

    On mobile footer, show policies first and All Rights Reserved last.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -63,7 +63,7 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className="mt-10 flex flex-col items-start justify-between gap-4 pt-6 text-[14px] font-medium text-[#999999] sm:mt-12 sm:flex-row sm:items-center">
+        <div className="mt-10 flex flex-col-reverse items-start justify-between gap-4 pt-6 text-[14px] font-medium text-[#999999] sm:mt-12 sm:flex-row sm:items-center">
           <p className="shrink-0">@2026 streamvib, All Rights Reserved</p>
           <div className="flex flex-wrap gap-x-4 gap-y-2 sm:gap-x-6">
             <Link
